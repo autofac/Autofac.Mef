4311d1525b5e0d95b69513ec6fcbdd1f95ab0476
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-01-16 01:34:02 +1000

    Package restore step added to MyGet build script.

--- MyGet.cmd
+++ MyGet.cmd
@@ -4,6 +4,10 @@ if "%config%" == "" (
    set config=Release
 )
 
+REM Package restore
+%nuget% install src\Autofac.Integration.Mef\packages.config -OutputDirectory %cd%\packages -NonInteractive
+%nuget% install tests\Autofac.Tests.Integration.Mef\packages.config -OutputDirectory %cd%\packages -NonInteractive
+
 REM Build
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Autofac.Mef.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
