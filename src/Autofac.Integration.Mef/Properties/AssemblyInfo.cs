690dfa334c1d5ecfdd2f8797185b35f2e3cacdf3
Alex Meyer-Gleaves <alex.meyergleaves@gmail.com>
2015-01-15 22:27:02 +1000

    Created a solution for Autofac.Mef and fixed up files missing from migration.

--- src/Autofac.Integration.Mef/Properties/AssemblyInfo.cs
+++ src/Autofac.Integration.Mef/Properties/AssemblyInfo.cs
@@ -1,9 +1,24 @@
 ﻿using System;
 using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 [assembly: AssemblyTitle("Autofac.Integration.Mef")]
 [assembly: InternalsVisibleTo("Autofac.Tests.Integration.Mef, PublicKey=00240000048000009400000006020000002400005253413100040000010001008728425885ef385e049261b18878327dfaaf0d666dea3bd2b0e4f18b33929ad4e5fbc9087e7eda3c1291d2de579206d9b4292456abffbe8be6c7060b36da0c33b883e3878eaf7c89fddf29e6e27d24588e81e86f3a22dd7b1a296b5f06fbfb500bbd7410faa7213ef4e2ce7622aefc03169b0324bcd30ccfe9ac8204e4960be6")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
+[assembly: AllowPartiallyTrustedCallers]
+
+[assembly: AssemblyCompany("Autofac Project - http://autofac.org")]
+[assembly: AssemblyProduct("Autofac")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
+[assembly: AssemblyConfiguration("Release built on 2014-01-07 21:40")]
+[assembly: AssemblyCopyright("Copyright © 2014 Autofac Contributors")]
+[assembly: AssemblyDescription("Autofac.Mef 3.0.2")]
