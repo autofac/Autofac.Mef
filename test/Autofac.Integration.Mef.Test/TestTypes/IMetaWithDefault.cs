f947608b0edfce2f7cb1ff3eaf02f3987abca109
Travis Illig <tillig@paraesthesia.com>
2021-08-19 08:00:49 -0700

    Standardized file headers.

--- test/Autofac.Integration.Mef.Test/TestTypes/IMetaWithDefault.cs
+++ test/Autofac.Integration.Mef.Test/TestTypes/IMetaWithDefault.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel;
+﻿// Copyright (c) Autofac Project. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System.ComponentModel;
 
 namespace Autofac.Integration.Mef.Test.TestTypes
 {
